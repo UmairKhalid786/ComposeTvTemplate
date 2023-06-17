cbf55afb05301b940f98d4fddef868de59dbb1ba
Umair Ali Khalid <umair.khalid786@outlook.com>
2023-06-18 00:43:06 +0200

    Animated text added at launcher

--- app/src/main/java/com/techlads/composetvtemplate/MainActivity.kt
+++ app/src/main/java/com/techlads/composetvtemplate/MainActivity.kt
@@ -2,12 +2,22 @@ package com.techlads.composetvtemplate
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.MaterialTheme
-import com.techlads.composetvtemplate.ui.Menu
 import com.techlads.composetvtemplate.ui.theme.ComposeTvTemplateTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : androidx.activity.ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +32,24 @@ class MainActivity : androidx.activity.ComponentActivity() {
 
 @Composable
 fun App() {
-    Menu()
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        HelloComposeTv()
+    }
 }
 
 @Composable
 fun HelloComposeTv() {
-    Text(text = "Hello Compose TV!", style = MaterialTheme.typography.headlineLarge)
+    var show by remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = Unit) {
+        delay(2000)
+        show = true
+    }
+    AnimatedVisibility(visible = show) {
+        Text(text = "Hello Compose TV!", style = MaterialTheme.typography.headlineLarge)
+    }
 }
 
 
